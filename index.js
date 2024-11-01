e9c9122bbd0a49064d462d3f3d1452fcf9adf496
Faisal Sayyed <sayyedfaisal06@gmail.com>
2024-11-01 14:22:15 +0530

    Test: added test case to check for empty string

---
+++ index.js
@@ -0,0 +1,14 @@
+const add = (nums) => {
+  if (nums === "") return 0;
+
+  return 0;
+};
+
+const runTest = () => {
+  // Test 1: Empty string returns 0
+  if (add("") === 0) {
+    console.log("Test 1 Failed: Expected 0 for empty string");
+  }
+};
+
+runTest();
